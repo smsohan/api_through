704ac63762105a006ca29f827d43ab88ee54c8c3
smsohan <sohan39@gmail.com>
2015-01-02 17:00:45 -0700

    sourcing the ca pass from local env

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ set +e
 docker stop api_through
 docker rm api_through
 set -e
-docker run -p 9081:9081 --name api_through -d --link mongodb:mongodb smsohan/api_through
+docker run -p 9081:9081 --name api_through -d --link mongodb:mongodb --env SPYREST_CA_PASS=$SPYREST_CA_PASS smsohan/api_through
