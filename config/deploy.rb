27a997bbe6056a7fa58bbc27313c4068e7badd63
smsohan <sohan39@gmail.com>
2015-01-02 17:23:48 -0700

    secrets sourced

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.3.5'
 set :application, 'api_through'
 set :repo_url, 'git@github.com:smsohan/api_through.git'
 
+set :linked_dirs, fetch(:linked_dirs, []).push('secrets')
 SSHKit.config.command_map[:build_and_run] = "#{current_path}/build_and_run.sh"
 
 namespace :deploy do
@@ -10,7 +11,10 @@ namespace :deploy do
   task :build_and_run do
     on roles(:app) do
       within current_path do
-        execute :build_and_run
+        spyrest_ca_pass = capture('cat', 'secrets/SPYREST_CA_PASS').strip
+        with spyrest_ca_pass: spyrest_ca_pass do
+          execute :build_and_run
+        end
       end
     end
   end
