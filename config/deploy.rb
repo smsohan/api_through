d79f4e744fd7ca9e7e72bca023633968777899d5
smsohan <sohan39@gmail.com>
2015-01-08 16:01:11 -0700

    faster deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,33 @@ SSHKit.config.command_map[:build_and_run] = "#{current_path}/build_and_run.sh"
 namespace :deploy do
 
   task :build_and_run do
+
     on roles(:app) do
+
+      last_packages = nil
+      last_release_dir = nil
+
+      within releases_path do
+        last_release = capture('ls', '-al | tail -2 | head -1')
+        if last_release
+          last_release_dir = last_release.strip.split.last
+          last_packages = capture('sha256sum', last_release_dir + '/package.json').strip.split.first
+        end
+      end
+
       within current_path do
+        if last_packages
+          current_gemfile_lock = capture('sha256sum', 'package.json').strip.split.first
+          last_if current_gemfile_lock == packages
+            puts "Copying the old Gemfile* since the contents are same"
+            execute :cp, "--preserve=timestamps #{releases_path}/#{last_release_dir}/package.json", "."
+          else
+            puts "Skipping the packages.json copy since the shas are different"
+          end
+        else
+          puts "Last release packages.json is not found"
+        end
+
         spyrest_ca_pass = capture('cat', 'secrets/SPYREST_CA_PASS').strip
         with spyrest_ca_pass: spyrest_ca_pass do
           execute :build_and_run
